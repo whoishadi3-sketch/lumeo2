d62c96496bc91ccbdd23461b66c264cbcecc5e40
whoishadi3-sketch <whoishadi3@gmail.com>
2025-11-08 13:13:47 +0000

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,7 +37,7 @@ export default function Home({ onNavigate }: HomeProps) {
           <Silk
             speed={8}
             scale={1}
-            color="#e3c519"
+            color="#780b6d"
             noiseIntensity={6}
             rotation={0}
           />
